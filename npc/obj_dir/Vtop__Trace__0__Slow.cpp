3a0b9a15ebb9e574c9f89dc53ab3cde020e32ef3
tracer-ysyx <tracer@ysyx.org>
2025-10-07 17:52:47 +0800

    > sim RTL ysyx_22040000 屈章鑫 Linux qzx 6.8.0-84-generic #84~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Sep  9 14:29:36 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:52:47 up  3:50,  3 users,  load average: 0.16, 0.13, 0.09

---
+++ npc/obj_dir/Vtop__Trace__0__Slow.cpp
@@ -0,0 +1,23 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Tracing implementation internals
+#include "verilated_vcd_c.h"
+#include "Vtop__Syms.h"
+
+
+VL_ATTR_COLD void Vtop___024root__trace_init_top(Vtop___024root* vlSelf, VerilatedVcd* tracep) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root__trace_init_top\n"); );
+    // Body
+    // Empty
+}
+
+void Vtop___024root__trace_cleanup(void* voidSelf, VerilatedVcd* /*unused*/);
+
+VL_ATTR_COLD void Vtop___024root__trace_register(Vtop___024root* vlSelf, VerilatedVcd* tracep) {
+    if (false && vlSelf) {}  // Prevent unused
+    Vtop__Syms* const __restrict vlSymsp VL_ATTR_UNUSED = vlSelf->vlSymsp;
+    VL_DEBUG_IF(VL_DBG_MSGF("+    Vtop___024root__trace_register\n"); );
+    // Body
+    tracep->addCleanupCb(&Vtop___024root__trace_cleanup, vlSelf);
+}
---
+++ npc/top.v
@@ -0,0 +1,3 @@
+module top;
+     initial begin $display("Hello World"); $finish; end
+  endmodule
