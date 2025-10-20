27abfc22b12407d2793af1554ca51af8400c15f6
tracer-ysyx <tracer@ysyx.org>
2025-10-20 21:13:45 +0800

    > compile NEMU ysyx_22040000 屈章鑫 Linux qzx 6.8.0-85-generic #85~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Sep 19 16:18:59 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:13:45 up  3:07,  1 user,  load average: 0.27, 0.29, 0.32

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -17,7 +17,6 @@
 #include <cpu/cpu.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include <memory/vaddr.h>
 #include "sdb.h"
 
 static int is_batch_mode = false;
@@ -56,7 +55,7 @@ static int cmd_q(char *args) {
 static int cmd_help(char *args);
 static int cmd_si(char *args);
 static int cmd_info(char *args);
-static int cmd_x(char *args);
+//static int cmd_x(char *args);
 
 static struct {
   const char *name;
@@ -67,8 +66,8 @@ static struct {
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
   { "si", "Step the execution of the program", cmd_si },
-  { "info", "Display information about registers or watchpoints", cmd_info },
-  { "x", "Examine memory", cmd_x },
+  { "info", "Display information about registers or watchpoints", cmd_info }
+  // { "x", "Examine memory", cmd_x },
   /* TODO: Add more commands */
 
 };
@@ -96,13 +95,7 @@ static int cmd_info(char *args) {
     if (reg_name == NULL) {
       isa_reg_display();
     } else {
-      bool success;
-      word_t val = isa_reg_str2val(reg_name, &success);
-      if (success) {
-        printf("%s: 0x%08x\n", reg_name, val);
-      } else {
-        printf("Unknown register '%s'\n", reg_name);
-      }
+      isa_reg_display_single(atoi(reg_name));
     }
   }
   else {
@@ -111,29 +104,7 @@ static int cmd_info(char *args) {
   return 0;
 }
 
-static int cmd_x(char *args) {
-  if (args == NULL) {
-    printf("Usage: x N ADDR - examine N words starting from ADDR\n");
-    return 0;
-  }
-  
-  char *n_str = strtok(args, " ");
-  char *addr_str = strtok(NULL, " ");
-  
-  if (n_str == NULL || addr_str == NULL) {
-    printf("Usage: x N ADDR - examine N words starting from ADDR\n");
-    return 0;
-  }
-  
-  int n = atoi(n_str);
-  vaddr_t addr = strtoul(addr_str, NULL, 0);
-  
-  for (int i = 0; i < n; i++) {
-    printf("0x%08x: 0x%08x\n", addr + i * 4, vaddr_read(addr + i * 4, 4));
-  }
-  
-  return 0;
-}
+
 
 static int cmd_help(char *args) {
   /* extract the first argument */
