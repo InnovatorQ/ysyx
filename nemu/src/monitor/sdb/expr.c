d026276f36de21f04ea50926682a565c81013866
tracer-ysyx <tracer@ysyx.org>
2025-10-30 18:51:25 +0800

    > compile NEMU ysyx_22040000 屈章鑫 Linux qzx 6.8.0-86-generic #87~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Sep 29 09:48:07 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:51:25 up  3:50,  1 user,  load average: 0.27, 0.16, 0.16

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -249,7 +249,7 @@ static word_t eval(int p, int q) {
       case '+': return val1 + val2;
       case '-': return val1 - val2;
       case '*': return val1 * val2;
-      case '/': return val2 == 0 ? 0 : val1 / val2;
+      case '/': return val2 == 0 ? 0 : (sword_t)val1 / (sword_t)val2;
       case TK_EQ: return val1 == val2;
       case TK_NE: return val1 != val2;
       case TK_AND: return val1 && val2;
