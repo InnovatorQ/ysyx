57611e3fe2a2f968ad09b784acf1ddbc32c36adf
tracer-ysyx <tracer@ysyx.org>
2025-11-06 21:34:57 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:34:57 up  2:29,  1 user,  load average: 0.74, 0.48, 0.40

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -19,24 +19,49 @@ char *strcpy(char *dst, const char *src) {
   char *temp = dst;
   assert((dst != NULL) && (src != NULL));
   while((*temp++ = *src++) != '\0');
-  return dst;
+  return dst;//返回首地址
   //panic("Not implemented");
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-
+  char *temp = dst;
+  assert((dst != NULL) && (src != NULL));
+  while(n){
+    *temp++ = *src++;
+    n--; 
+  }
+  return dst;
   panic("Not implemented");
 }
 
 char *strcat(char *dst, const char *src) {
+  char *temp = dst;
+  assert((dst != NULL) && (src != NULL));
+  while(*temp != '\0'){
+    temp++;
+  }
+  while((*temp++ = *src++) != '\0');
+  return dst;
   panic("Not implemented");
 }
 
 int strcmp(const char *s1, const char *s2) {
+  assert((s1 != NULL) && (s2 != NULL));
+  while(*s1 == *s2){
+    if(*s1 == '\0') return 0;
+    s1++; s2++;
+  }
+  return *(unsigned char*)s1 - *(unsigned char*)s2;
   panic("Not implemented");
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
+  assert((s1 != NULL) && (s2 != NULL));
+  if(n == 0) return 0;
+  while((--n && *s1) && (*s1 == *s2)){
+    s1++; s2++;
+  }
+  return *(unsigned char*)s1 - *(unsigned char*)s2;
   panic("Not implemented");
 }
 
