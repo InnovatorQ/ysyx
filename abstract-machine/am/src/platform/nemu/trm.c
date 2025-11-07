f1aa96c3a7042bf8ac5af92ba73b2b6b5c5859c2
Qzx <1944332766@qq.com>
2025-11-07 14:09:33 +0800

    feat:完成PA2中的程序，运行时环境与AM节

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -3,21 +3,21 @@
 
 extern char _heap_start;
 int main(const char *args);
-
+//用于指示堆区的起始和末尾
+//堆区是给程序自由使用的一段内存区间, 为程序提供动态分配内存的功能
 Area heap = RANGE(&_heap_start, PMEM_END);
 static const char mainargs[MAINARGS_MAX_LEN] = TOSTRING(MAINARGS_PLACEHOLDER); // defined in CFLAGS
-
+//用于输出字符
 void putch(char ch) {
   outb(SERIAL_PORT, ch);
 }
-
+//用于结束程序的运行
 void halt(int code) {
-  nemu_trap(code);
-
+  nemu_trap(code); // (在abstract-machine/am/src/platform/nemu/include/nemu.h中定义），code为结束码
   // should not reach here
   while (1);
 }
-
+//用于进行图灵机相关的初始化工作
 void _trm_init() {
   int ret = main(mainargs);
   halt(ret);
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -5,43 +5,116 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  assert(s != NULL);
+  size_t len = 0;
+  while(*s != '\0'){
+    len++;
+    s++;
+  }
+  return len;
+  //panic("Not implemented");
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  char *temp = dst;
+  assert((dst != NULL) && (src != NULL));
+  while((*temp++ = *src++) != '\0');
+  return dst;//返回首地址
+  //panic("Not implemented");
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  char *temp = dst;
+  assert((dst != NULL) && (src != NULL));
+  while(n && (*temp = *src)){
+    temp++; src++; n--;
+  }
+  while(n--){
+    *temp++ = '\0';
+  }
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  char *temp = dst;
+  assert((dst != NULL) && (src != NULL));
+  while(*temp != '\0'){
+    temp++;
+  }
+  while((*temp++ = *src++) != '\0');
+  return dst;
+  //panic("Not implemented");
 }
 
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  assert((s1 != NULL) && (s2 != NULL));
+  while(*s1 && (*s1 == *s2)){
+    s1++; s2++;
+  }
+  return *(unsigned char*)s1 - *(unsigned char*)s2;
+  //panic("Not implemented");
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  assert((s1 != NULL) && (s2 != NULL));
+  if(n == 0) return 0;
+  while((--n && *s1) && (*s1 == *s2)){
+    s1++; s2++;
+  }
+  return *(unsigned char*)s1 - *(unsigned char*)s2;
+  //panic("Not implemented");
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  assert(s != NULL);
+  const unsigned char unsigned_c = c;
+  unsigned char *temp = s;
+  while(n--){
+    *temp++ = unsigned_c;
+  }
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  assert((dst != NULL) && (src != NULL));
+  char *pd = dst;
+  const char *ps = src;
+  void *ret = dst;
+
+  if(pd <= ps || pd >= ps + n){
+    while(n--){
+      *pd++ = *ps++;
+    }
+  }else{
+    pd += n - 1;
+    ps += n - 1;
+    while(n--){
+      *pd-- = *ps--;
+    }
+  }
+  return ret;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  assert((out != NULL) && (in != NULL));
+  char *d = out;
+  const char *s = in;
+  while(n--){
+    *d++ = *s++;
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  assert((s1 != NULL) && (s2 != NULL));
+  const unsigned char *p1 = s1, *p2 = s2;
+  while(n--){
+    if(*p1 != *p2){
+      return (int)(*p1 - *p2);
+    }
+    p1++; p2++; 
+  }
+  return 0;
 }
 
 #endif
