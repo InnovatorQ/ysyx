9c75dd9a70e9b52a3344ec8fc503061aa470f8b7
tracer-ysyx <tracer@ysyx.org>
2025-11-07 12:57:23 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:57:23 up 17:52,  1 user,  load average: 0.26, 0.26, 0.16

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -47,8 +47,7 @@ char *strcat(char *dst, const char *src) {
 
 int strcmp(const char *s1, const char *s2) {
   assert((s1 != NULL) && (s2 != NULL));
-  while(*s1 == *s2){
-    if(*s1 == '\0') return 0;
+  while(*s1 && (*s1 == *s2)){
     s1++; s2++;
   }
   return *(unsigned char*)s1 - *(unsigned char*)s2;
