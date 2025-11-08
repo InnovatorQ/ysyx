dd0dee29f0f87e94481d2e73b18202e2fc40ae46
tracer-ysyx <tracer@ysyx.org>
2025-11-08 11:41:14 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:41:14 up 1 day, 16:36,  1 user,  load average: 0.85, 0.69, 0.46

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -52,14 +52,45 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
+  if (likely(in_pmem(addr))){ 
+    word_t ret = pmem_read(addr, len);
+    #ifdef CONFIG_MTRACE
+    #ifdef CONFIG_MTRACE_COND
+    if(MTRACE_COND){
+      log_write("MTRACE: READ [" FMT_PADDR"] = " FMT_WORD " (len=%d) at pc=" FMT_WORD "\n",
+      addr, ret, len, cpu.pc);
+    }
+    #endif
+    #endif 
+    return ret;
+  }
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
-  if (likely(in_pmem(addr))) { pmem_write(addr, len, data); return; }
-  IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
+  if (likely(in_pmem(addr))) { 
+    pmem_write(addr, len, data); 
+    #ifdef CONFIG_MTRACE
+    #ifdef CONFIG_MTRACE_COND
+    if (MTRACE_COND) {
+      log_write("MTRACE: WRITE [" FMT_PADDR "] = " FMT_WORD " (len=%d) at pc=" FMT_WORD "\n", 
+        addr, data, len, cpu.pc);
+    }
+    #endif
+    #endif
+    return; 
+  }
+  IFDEF(CONFIG_DEVICE, //保持对MMIO的跟踪
+  #ifdef CONFIG_MTRACE
+  #ifdef CONFIG_MTRACE_COND
+      if(MTRACE_COND){
+        log_write("MTRACE: WRITE [" FMT_PADDR "] = " FMT_WORD " (len=%d) at pc=" FMT_WORD " [MMIO]\n",
+          addr, data, len, cpu.pc);
+      }
+  #endif
+  #endif
+      mmio_write(addr, len, data); return);
   out_of_bound(addr);
 }
