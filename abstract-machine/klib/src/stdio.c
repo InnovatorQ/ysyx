ec4e84e2e61bb7b09fd7d2b281079e3d471d5180
tracer-ysyx <tracer@ysyx.org>
2025-11-08 12:26:00 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:26:00 up 1 day, 17:20,  1 user,  load average: 0.39, 0.41, 0.44

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -15,54 +15,53 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  // va_list args;
-  // va_start(args, fmt);
-  // char *pbuf = out;
-  // const char *pf = fmt;
+  va_list args;
+  va_start(args, fmt);
+  char *pbuf = out;
+  const char *pf = fmt;
   
-  // while(*pf){
-  //   if(*pf != '%'){
-  //     *pbuf++ = *pf++;
-  //     continue;
-  //   }
-  //   pf++;
-  //   switch (*pf) {
-  //     case 'd': {
-  //       int num = va_arg(args, int);
-  //       if(num < 0) {
-  //         *pbuf++ = '-';
-  //         num = -num;
-  //       }
-  //       char temp[12];
-  //       int i = 0;
-  //       if(num == 0) temp[i++] = '0';
-  //       while(num > 0) {
-  //         temp[i++] = '0' + num % 10;
-  //         num /= 10;
-  //       }
-  //       while(--i >= 0) *pbuf++ = temp[i];
-  //       break;
-  //     }
-  //     case 's': {
-  //       char *str = va_arg(args, char*);
-  //       while(*str) *pbuf++ = *str++;
-  //       break;
-  //     }
-  //     case 'c': {
-  //       *pbuf++ = va_arg(args, int);
-  //       break;
-  //     }
-  //     case '%': {
-  //       *pbuf++ = '%';
-  //       break;
-  //     }
-  //   }
-  //   pf++;
-  // }
-  // *pbuf = '\0';
-  // va_end(args);
-  // return pbuf - out;
-  panic("Not implemented");
+  while(*pf){
+    if(*pf != '%'){
+      *pbuf++ = *pf++;
+      continue;
+    }
+    pf++;
+    switch (*pf) {
+      case 'd': {
+        int num = va_arg(args, int);
+        if(num < 0) {
+          *pbuf++ = '-';
+          num = -num;
+        }
+        char temp[12];
+        int i = 0;
+        if(num == 0) temp[i++] = '0';
+        while(num > 0) {
+          temp[i++] = '0' + num % 10;
+          num /= 10;
+        }
+        while(--i >= 0) *pbuf++ = temp[i];
+        break;
+      }
+      case 's': {
+        char *str = va_arg(args, char*);
+        while(*str) *pbuf++ = *str++;
+        break;
+      }
+      case 'c': {
+        *pbuf++ = va_arg(args, int);
+        break;
+      }
+      case '%': {
+        *pbuf++ = '%';
+        break;
+      }
+    }
+    pf++;
+  }
+  *pbuf = '\0';
+  va_end(args);
+  return pbuf - out;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
