ddfce040d0ab4a06db4a9f6158ef4eafa18cf59a
tracer-ysyx <tracer@ysyx.org>
2025-11-09 15:38:54 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:38:54 up  3:49,  1 user,  load average: 0.49, 0.52, 0.46

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -19,7 +19,7 @@
 // 把通用寄存器和PC与从DUT中读出的寄存器的值进行比较. 若对比结果一致, 函数返回true; 如果发现值不一样, 函数返回false
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   // 检查PC
-  if (ref_r->pc != pc) {
+  if (ref_r->pc != cpu.pc) {
     printf("PC mismatch: " FMT_WORD "\n", pc);
     return false;
   }
