c5446491fd9d686d224af0d0055661192df38893
tracer-ysyx <tracer@ysyx.org>
2025-11-16 07:35:47 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:35:47 up  1:17,  1 user,  load average: 0.34, 0.44, 0.51

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -34,7 +34,15 @@ void *malloc(size_t size) {
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
 #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
+  // static char *addr 在函数调用间保持上次分配的位置，避免重复分配同一块内存
+  static char *addr = NULL;
+  if (addr == NULL) { // 第一次调用malloc时，初始化addr指向heap的起始位置
+    addr = (char *)ROUNDUP(heap.start, 8);
+  }
+  size = ROUNDUP(size, 8); // 确保每次分配的大小是8的倍数
+  char *ret = addr;
+  addr += size;
+  return ret;
 #endif
   return NULL;
 }
