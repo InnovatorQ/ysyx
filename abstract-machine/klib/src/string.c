d4b5775238cae18704499c6781fb08b32fd7a294
tracer-ysyx <tracer@ysyx.org>
2025-11-16 08:20:20 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:20:20 up  2:02,  1 user,  load average: 0.50, 0.43, 0.48

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -67,7 +67,7 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 
 void *memset(void *s, int c, size_t n) {
   assert(s != NULL);
-  const unsigned char unsigned_c = c;
+  unsigned char unsigned_c = c;
   unsigned char *temp = s;
   while(n--){
     *temp++ = unsigned_c;
