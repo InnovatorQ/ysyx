7305de279abb2f7f6c47e28013090d1aaa1cd5d8
tracer-ysyx <tracer@ysyx.org>
2025-11-16 11:03:04 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:03:04 up  4:44,  1 user,  load average: 0.17, 0.37, 0.41

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -23,7 +23,7 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   uint32_t *pixels = (uint32_t *)ctl->pixels;
-  int screen_w = inl(VGACTL_ADDR) & 0xffff;
+  int screen_w = inl(VGACTL_ADDR) & 0xff;
   // 一行一行地将pixels中的图像数据复制到显存fb的对应位置
   for (int j = 0; j < ctl->h; j++) {
     for (int i = 0; i < ctl->w; i++) {
