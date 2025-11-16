592cb08c46d1154be56f93ef49371e6ea3393cd7
tracer-ysyx <tracer@ysyx.org>
2025-11-16 10:22:14 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:22:14 up  4:04,  1 user,  load average: 0.39, 0.31, 0.41

--- abstract-machine/am/include/amdev.h
+++ abstract-machine/am/include/amdev.h
@@ -18,8 +18,10 @@ AM_DEVREG( 6, TIMER_UPTIME, RD, uint64_t us);
 AM_DEVREG( 7, INPUT_CONFIG, RD, bool present);
 // AM键盘控制器, 可读出按键信息. 
 AM_DEVREG( 8, INPUT_KEYBRD, RD, bool keydown; int keycode);
+// AM显示控制器信息, 可读出屏幕大小信息width和height. 
 AM_DEVREG( 9, GPU_CONFIG,   RD, bool present, has_accel; int width, height, vmemsz);
 AM_DEVREG(10, GPU_STATUS,   RD, bool ready);
+// AM帧缓冲控制器, 可写入绘图信息, 向屏幕(x, y)坐标处绘制w*h的矩形图像. 
 AM_DEVREG(11, GPU_FBDRAW,   WR, int x, y; void *pixels; int w, h; bool sync);
 AM_DEVREG(12, GPU_MEMCPY,   WR, uint32_t dest; void *src; int size);
 AM_DEVREG(13, GPU_RENDER,   WR, uint32_t root);
--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -2,7 +2,7 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
-//#define KBD_ADDR        (DEVICE_BASE + 0x0000060)
+// KBD_ADDR        (DEVICE_BASE + 0x0000060)
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t key_data = inl(KBD_ADDR);
