c7aee7cb714e1e026e82b39347d991e1ea457c22
tracer-ysyx <tracer@ysyx.org>
2025-12-01 12:42:40 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:42:40 up  1:31,  1 user,  load average: 0.62, 0.55, 0.35

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -189,7 +189,7 @@ __instpat_end_: ; }
   });
   INSTPAT("0011000 00010 00000 000 00000 11100 11", mret   , N, {
     s->dnpc = mepc;
-    mstatus = 0x0;
+    
   });
   INSTPAT("0000000 00000 00000 000 00000 11100 11", ecall  , N, s->dnpc = isa_raise_intr(11, s->pc));
   INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
