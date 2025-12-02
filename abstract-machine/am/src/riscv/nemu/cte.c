593bb4d165e23776ab46f1931cb8ac6190e7bf7e
tracer-ysyx <tracer@ysyx.org>
2025-12-02 22:36:13 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:36:13 up 23:47,  1 user,  load average: 0.71, 0.51, 0.54

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -43,8 +43,8 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   //创建新的线程时需要初始化线程的上下文信息
   memset(cp, 0, sizeof(Context));
   assert((kstack.end - (void *)cp) == sizeof(Context));
-  cp->gpr[10] = (uintptr_t)arg; //设置a0寄存器传递参数
   cp->mepc = (uintptr_t)entry; //设置内核线程入口
+  cp->gpr[10] = (uintptr_t)arg; //设置a0寄存器传递参数
   cp->mstatus = 0x00001800; // MIE=1, MPIE=1
   return cp;
   //return NULL;
