2b73a09a7ee106a7dddaa66404a5c589f8065fdd
tracer-ysyx <tracer@ysyx.org>
2025-12-02 22:16:53 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:16:53 up 23:28,  1 user,  load average: 0.67, 0.52, 0.52

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -12,7 +12,7 @@ Context* __am_irq_handle(Context *c) {
       default: ev.event = EVENT_ERROR; break;
     }
     for(int i = 0 ; i < 32; i++){
-      printf("GPR[%d]=0x%x ", i, (uint32_t)c->gpr[i]);
+      printf("GPR[%d]=0x%x\n ", i, (uint32_t)c->gpr[i]);
     }
     c = user_handler(ev, c);
     assert(c != NULL);
