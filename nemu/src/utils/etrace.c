ffb9cc714612af2fa3c75526df9de456744fefed
tracer-ysyx <tracer@ysyx.org>
2025-12-09 16:44:03 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-87-generic #88~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Oct 14 14:03:14 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:44:03 up 1 day,  6:25,  1 user,  load average: 0.44, 0.54, 0.50

--- nemu/src/utils/etrace.c
+++ nemu/src/utils/etrace.c
@@ -7,8 +7,8 @@ static const char *exception_names[] = {
 void etrace_exception_entry(word_t cause, vaddr_t pc, vaddr_t handler) {
   const char *name = (cause < sizeof(exception_names)/sizeof(char*) && exception_names[cause]) 
                      ? exception_names[cause] : "Unknown";
-  //printf("ETRACE: exception entry [%s] at pc = " FMT_WORD ", handler = " FMT_WORD "\n", 
-  //       name, pc, handler);
+  printf("ETRACE: exception entry [%s] at pc = " FMT_WORD ", handler = " FMT_WORD "\n", 
+         name, pc, handler);
   log_write("ETRACE: exception entry [%s] at pc = " FMT_WORD ", handler = " FMT_WORD "\n", 
          name, pc, handler);
 }
