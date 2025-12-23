f97101506b2f0dca607af6de732fc9b1f3b18eb3
tracer-ysyx <tracer@ysyx.org>
2025-12-23 13:39:55 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:39:55 up  1:04,  1 user,  load average: 0.52, 0.94, 0.74

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -26,7 +26,7 @@ image: image-dep
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: insert-arg
-	$(MAKE) -C $(NPC_HOME) run OBJ_TAR=$(IMAGE).bin ELF_TAR=$(IMAGE).elf
-    
-
-.PHONY: insert-arg run
+	$(MAKE) -C $(NPC_HOME) run OBJ_TAR=$(IMAGE).bin ELF_TAR=$(IMAGE).elf DST_DIR=$(DST_DIR)
+sim: insert-arg
+	$(MAKE) -C $(NPC_HOME) sim DST_DIR=$(DST_DIR)
+.PHONY: insert-arg run sim
