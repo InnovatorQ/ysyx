56e9ba4d30b62cc0f85d53b3d783bba4a4cf08c6
tracer-ysyx <tracer@ysyx.org>
2026-02-01 16:49:57 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:49:57 up 46 min,  1 user,  load average: 0.12, 0.16, 0.25

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -25,8 +25,8 @@ static uint8_t *pmem = NULL;
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 
-static uint8_t mrom[0x1000] = {};
-static uint8_t sram[0x2000] = {};
+static uint8_t mrom[0x1000] = {}; // 4KB 
+static uint8_t sram[0x2000] = {}; // 8KB
 //实现从虚拟地址到物理地址的转换
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 //实现从物理地址到虚拟地址的转换
