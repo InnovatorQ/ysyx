df36f34a53ebd5be8e77f83570910de52c591359
tracer-ysyx <tracer@ysyx.org>
2026-02-05 19:50:33 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-90-generic #91~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 15:20:45 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:50:33 up 2 days,  5:08,  1 user,  load average: 0.41, 0.54, 0.88

--- abstract-machine/am/src/riscv/ysyxsoc/trm.c
+++ abstract-machine/am/src/riscv/ysyxsoc/trm.c
@@ -17,7 +17,7 @@ Area heap = RANGE(&_heap_start, PMEM_END);
 static const char* mainargs = " ";
 
 void putch(char ch) {
-  while((*((volatile char *)UART_BASE + UART_LSR) & (1 << 5))); 
+  while((*((volatile char *)UART_BASE + UART_LSR) & (1 << 5)) == 0); 
   *((volatile char *)UART_BASE) = ch;
 }
 
