065598a4b0d9a73460584c434b3eb6ede62833bd
tracer-ysyx <tracer@ysyx.org>
2026-02-23 10:18:47 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:18:47 up 1 day, 18:59,  1 user,  load average: 0.40, 0.39, 0.30

--- ysyx_25110269/csrc/sim_main.cpp
+++ ysyx_25110269/csrc/sim_main.cpp
@@ -129,19 +129,19 @@ static void reset(int n){
 }
 
 static void show_pref(){
-    Log("----------------------------------------------------");
+    Log("---------------------------------------------------");
     Log("|Total prefetched instructions \t| %d  \t|", CPU_INFO(IFU__DOT__pref_cnt));
-    Log("-------------------------------------------");
+    Log("---------------------------------------------------");
     Log("|decode calculate prefetch     \t| %d  \t\t|", CPU_INFO(IDU__DOT__pref_cnt_alu));
     Log("|decode load/store prefetch    \t| %d  \t\t|", CPU_INFO(IDU__DOT__pref_cnt_ls));
     Log("|decode branch prefetch        \t| %d  \t\t|", CPU_INFO(IDU__DOT__pref_cnt_br));
     Log("|decode csr prefetch           \t| %d  \t\t|", CPU_INFO(IDU__DOT__pref_cnt_csr));
-    Log("-------------------------------------------");
+    Log("---------------------------------------------------");
     Log("|LSU prefetch                  \t| %d  \t\t|", CPU_INFO(LSU__DOT__pref_cnt));
     Log("|LSU delay cycles              \t| %d  \t|", CPU_INFO(LSU__DOT__delay_cnt));
-    Log("-------------------------------------------");
+    Log("---------------------------------------------------");
     Log("|EXU prefetch                  \t| %d  \t|", CPU_INFO(EXU__DOT__pref_cnt));
-    Log("----------------------------------------------------");
+    Log("---------------------------------------------------");
 }
 
 word_t get_rf(int n){
