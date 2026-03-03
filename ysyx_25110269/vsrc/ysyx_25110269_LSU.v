9ecca632109fa928807223568502dc52fb16ade6
tracer-ysyx <tracer@ysyx.org>
2026-03-03 14:36:41 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-94-generic #96~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Jan 16 13:19:05 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:36:41 up 9 days, 23:17,  1 user,  load average: 0.74, 0.44, 0.36

--- ysyx_25110269/vsrc/ysyx_25110269_LSU.v
+++ ysyx_25110269/vsrc/ysyx_25110269_LSU.v
@@ -143,7 +143,7 @@ module ysyx_25110269_LSU(
                     (load == 4'h3) ? 3'b001 : 
                     (load == 4'h1) ? 3'b000 : 3'b0;
     assign arlen  = 8'h0;
-    assign ms_to_ws_valid = (bready & bvalid) || !is_ls || (ms_state == ms_rdata_ready);
+    assign ms_to_ws_valid = (bready & bvalid) || ((ms_state == ms_idle) && !is_ls) || (ms_state == ms_rdata_ready);
 
     assign awvalid = mem_wen & (ms_state == ms_wait_ready);
     assign wvalid  = mem_wen & (ms_state == ms_wait_ready);
