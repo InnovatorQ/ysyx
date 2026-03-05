ba815a002bb3ed53b9de4fc649e553e0a515b9ec
tracer-ysyx <tracer@ysyx.org>
2026-03-05 10:42:06 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-101-generic #101~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Feb 11 13:19:54 UTC  x86_64 x86_64 x86_64 GNU/Linux  10:42:06 up 1 day, 13:57,  1 user,  load average: 0.08, 0.15, 0.11

--- ysyx_25110269/vsrc/ysyx_25110269_IFU.v
+++ ysyx_25110269/vsrc/ysyx_25110269_IFU.v
@@ -65,7 +65,7 @@ module ysyx_25110269_IFU(
     end
     
     
-    assign arvalid = (fs_state == fs_wait_ready);
+    assign arvalid = (fs_state == fs_wait_ready) && !br_taken;
     assign araddr = pc;
     
     assign fs_to_ds_valid = (fs_state == fs_data_ready) && !br_taken;
