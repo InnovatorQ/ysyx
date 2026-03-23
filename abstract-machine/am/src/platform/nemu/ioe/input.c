324186e5e40b54cc1aa4dd283ed7593571b7ff11
tracer-ysyx <tracer@ysyx.org>
2026-03-23 15:05:27 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  15:05:27 up  3:44,  1 user,  load average: 0.36, 0.49, 0.62

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -2,7 +2,6 @@
 #include <nemu.h>
 
 #define KEYDOWN_MASK 0x8000
-// KBD_ADDR        (DEVICE_BASE + 0x0000060)
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   uint32_t key_data = inl(KBD_ADDR);
