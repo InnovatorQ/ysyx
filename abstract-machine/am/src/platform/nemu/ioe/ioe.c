0aa0d50a39f3b4024a541ae11e47b212061085cb
tracer-ysyx <tracer@ysyx.org>
2026-03-24 17:08:51 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  17:08:51 up 1 day,  5:47,  1 user,  load average: 1.08, 0.87, 0.85

--- abstract-machine/am/src/platform/nemu/ioe/ioe.c
+++ abstract-machine/am/src/platform/nemu/ioe/ioe.c
@@ -25,56 +25,6 @@ static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
 static void __am_uart_config(AM_UART_CONFIG_T *cfg)   { cfg->present = true; }
 static void __am_net_config (AM_NET_CONFIG_T *cfg)    { cfg->present = false; }
 
-static int shift_pressed = 0;
-static const uint8_t keymap[] = {
-  [AM_KEY_ESCAPE] = 27,
-
-  [AM_KEY_GRAVE]  = '`',
-  [AM_KEY_1] = '1',
-  [AM_KEY_2] = '2',
-  [AM_KEY_3] = '3',
-  [AM_KEY_4] = '4',
-  [AM_KEY_5] = '5',
-  [AM_KEY_6] = '6',
-  [AM_KEY_7] = '7',
-  [AM_KEY_8] = '8',
-  [AM_KEY_9] = '9',
-  [AM_KEY_0] = '0',
-  [AM_KEY_MINUS]  = '-',
-  [AM_KEY_EQUALS] = '=',
-  [AM_KEY_BACKSPACE] = '\b',
-
-  [AM_KEY_TAB] = '\t',
-
-  [AM_KEY_Q]='q',[AM_KEY_W]='w',[AM_KEY_E]='e',[AM_KEY_R]='r',
-  [AM_KEY_T]='t',[AM_KEY_Y]='y',[AM_KEY_U]='u',[AM_KEY_I]='i',
-  [AM_KEY_O]='o',[AM_KEY_P]='p',
-
-  [AM_KEY_LEFTBRACKET]='[',
-  [AM_KEY_RIGHTBRACKET]=']',
-  [AM_KEY_BACKSLASH]='\\',
-
-  [AM_KEY_A]='a',[AM_KEY_S]='s',[AM_KEY_D]='d',[AM_KEY_F]='f',
-  [AM_KEY_G]='g',[AM_KEY_H]='h',[AM_KEY_J]='j',[AM_KEY_K]='k',
-  [AM_KEY_L]='l',
-
-  [AM_KEY_SEMICOLON]=';',
-  [AM_KEY_APOSTROPHE]='\'',
-  [AM_KEY_RETURN]='\n',
-
-  [AM_KEY_Z]='z',[AM_KEY_X]='x',[AM_KEY_C]='c',
-  [AM_KEY_V]='v',[AM_KEY_B]='b',[AM_KEY_N]='n',
-  [AM_KEY_M]='m',
-
-  [AM_KEY_COMMA]=',',
-  [AM_KEY_PERIOD]='.',
-  [AM_KEY_SLASH]='/',
-
-  [AM_KEY_SPACE]=' ',
-
-  [AM_KEY_DELETE]=127,
-};
-
 typedef void (*handler_t)(void *buf);
 static void *lut[128] = {
   [AM_TIMER_CONFIG] = __am_timer_config,
@@ -106,68 +56,6 @@ bool ioe_init() {
   __am_timer_init();
   __am_audio_init();
   return true;
-}
-
-void __am_uart_rx(AM_UART_RX_T *rx) {
-   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
-
-  if (ev.keycode == AM_KEY_NONE) {
-    rx->data = 0xff;
-    return;
-  }
-
-  if (ev.keycode == AM_KEY_LSHIFT ||
-      ev.keycode == AM_KEY_RSHIFT) {
-    shift_pressed = ev.keydown;
-    rx->data = 0xff;
-    return;
-  }
-
-  if (!ev.keydown) {
-    rx->data = 0xff;
-    return;
-  }
-
-  uint8_t ch = keymap[ev.keycode];
-  if (ch == 0) {
-    rx->data = 0xff;
-    return;
-  }
-
-  if (shift_pressed) {
-
-    if (ch >= 'a' && ch <= 'z') {
-      ch = ch - 'a' + 'A';
-    }
-    else {
-      switch (ch) {
-        case '-': ch = '_'; break;
-        case '=': ch = '+'; break;
-        case '[': ch = '{'; break;
-        case ']': ch = '}'; break;
-        case '\\': ch = '|'; break;
-        case ';': ch = ':'; break;
-        case '\'': ch = '"'; break;
-        case ',': ch = '<'; break;
-        case '.': ch = '>'; break;
-        case '/': ch = '?'; break;
-        case '`': ch = '~'; break;
-
-        case '1': ch = '!'; break;
-        case '2': ch = '@'; break;
-        case '3': ch = '#'; break;
-        case '4': ch = '$'; break;
-        case '5': ch = '%'; break;
-        case '6': ch = '^'; break;
-        case '7': ch = '&'; break;
-        case '8': ch = '*'; break;
-        case '9': ch = '('; break;
-        case '0': ch = ')'; break;
-      }
-    }
-  }
-
-  rx->data = ch;
 }
 
 void ioe_read (int reg, void *buf) { ((handler_t)lut[reg])(buf); }
