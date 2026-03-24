a048e28c7dbca4b261342cc813d4f1dfa8a1f516
tracer-ysyx <tracer@ysyx.org>
2026-03-24 11:11:24 +0800

    > compile NEMU ysyx_25110269 屈章鑫 Linux qzx 6.8.0-106-generic #106~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar  6 08:44:59 UTC  x86_64 x86_64 x86_64 GNU/Linux  11:11:24 up 23:50,  1 user,  load average: 0.51, 0.58, 0.52

--- abstract-machine/am/src/platform/nemu/ioe/ioe.c
+++ abstract-machine/am/src/platform/nemu/ioe/ioe.c
@@ -18,6 +18,7 @@ void __am_audio_play(AM_AUDIO_PLAY_T *);
 void __am_disk_config(AM_DISK_CONFIG_T *cfg);
 void __am_disk_status(AM_DISK_STATUS_T *stat);
 void __am_disk_blkio(AM_DISK_BLKIO_T *io);
+void __am_uart_rx(AM_UART_RX_T *rx);
 
 static void __am_timer_config(AM_TIMER_CONFIG_T *cfg) { cfg->present = true; cfg->has_rtc = true; }
 static void __am_input_config(AM_INPUT_CONFIG_T *cfg) { cfg->present = true;  }
@@ -73,6 +74,40 @@ static const uint8_t keymap[] = {
 
   [AM_KEY_DELETE]=127,
 };
+
+typedef void (*handler_t)(void *buf);
+static void *lut[128] = {
+  [AM_TIMER_CONFIG] = __am_timer_config,
+  [AM_TIMER_RTC   ] = __am_timer_rtc,
+  [AM_TIMER_UPTIME] = __am_timer_uptime,
+  [AM_INPUT_CONFIG] = __am_input_config,
+  [AM_INPUT_KEYBRD] = __am_input_keybrd,
+  [AM_GPU_CONFIG  ] = __am_gpu_config,
+  [AM_GPU_FBDRAW  ] = __am_gpu_fbdraw,
+  [AM_GPU_STATUS  ] = __am_gpu_status,
+  [AM_UART_CONFIG ] = __am_uart_config,
+  [AM_UART_RX     ] = __am_uart_rx,
+  [AM_AUDIO_CONFIG] = __am_audio_config,
+  [AM_AUDIO_CTRL  ] = __am_audio_ctrl,
+  [AM_AUDIO_STATUS] = __am_audio_status,
+  [AM_AUDIO_PLAY  ] = __am_audio_play,
+  [AM_DISK_CONFIG ] = __am_disk_config,
+  [AM_DISK_STATUS ] = __am_disk_status,
+  [AM_DISK_BLKIO  ] = __am_disk_blkio,
+  [AM_NET_CONFIG  ] = __am_net_config,
+};
+
+static void fail(void *buf) { panic("access nonexist register"); }
+
+bool ioe_init() {
+  for (int i = 0; i < LENGTH(lut); i++)
+    if (!lut[i]) lut[i] = fail;
+  __am_gpu_init();
+  __am_timer_init();
+  __am_audio_init();
+  return true;
+}
+
 void __am_uart_rx(AM_UART_RX_T *rx) {
    AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
 
@@ -133,39 +168,6 @@ void __am_uart_rx(AM_UART_RX_T *rx) {
   }
 
   rx->data = ch;
-}
-
-typedef void (*handler_t)(void *buf);
-static void *lut[128] = {
-  [AM_TIMER_CONFIG] = __am_timer_config,
-  [AM_TIMER_RTC   ] = __am_timer_rtc,
-  [AM_TIMER_UPTIME] = __am_timer_uptime,
-  [AM_INPUT_CONFIG] = __am_input_config,
-  [AM_INPUT_KEYBRD] = __am_input_keybrd,
-  [AM_GPU_CONFIG  ] = __am_gpu_config,
-  [AM_GPU_FBDRAW  ] = __am_gpu_fbdraw,
-  [AM_GPU_STATUS  ] = __am_gpu_status,
-  [AM_UART_CONFIG ] = __am_uart_config,
-  [AM_UART_RX     ] = __am_uart_rx,
-  [AM_AUDIO_CONFIG] = __am_audio_config,
-  [AM_AUDIO_CTRL  ] = __am_audio_ctrl,
-  [AM_AUDIO_STATUS] = __am_audio_status,
-  [AM_AUDIO_PLAY  ] = __am_audio_play,
-  [AM_DISK_CONFIG ] = __am_disk_config,
-  [AM_DISK_STATUS ] = __am_disk_status,
-  [AM_DISK_BLKIO  ] = __am_disk_blkio,
-  [AM_NET_CONFIG  ] = __am_net_config,
-};
-
-static void fail(void *buf) { panic("access nonexist register"); }
-
-bool ioe_init() {
-  for (int i = 0; i < LENGTH(lut); i++)
-    if (!lut[i]) lut[i] = fail;
-  __am_gpu_init();
-  __am_timer_init();
-  __am_audio_init();
-  return true;
 }
 
 void ioe_read (int reg, void *buf) { ((handler_t)lut[reg])(buf); }
